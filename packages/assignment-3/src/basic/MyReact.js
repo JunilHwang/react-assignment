17f826e04babcd28a9b19f0d7d07d71c6af8498d
junilhwang <junil.h@kakao.com>
2024-06-02 01:53:46 +0900

    feat: assignment 3 솔루션 추가

--- packages/assignment-3/src/basic/MyReact.js
+++ packages/assignment-3/src/basic/MyReact.js
@@ -2,8 +2,30 @@ import { createHooks } from "./hooks";
 import { render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  const renderContext = {
+    $root: null,
+    rootComponent: null,
+    currentNode: null,
+    beforeNode: null,
+  }
+
+  const _render = () => {
+    const currentNode = renderContext.rootComponent();
+
+    updateElement(renderContext.$root, currentNode, renderContext.currentNode);
+    resetHookContext();
+
+    renderContext.beforeNode = renderContext.currentNode;
+    renderContext.currentNode = currentNode;
+  };
+
+  function render($root, rootComponent) {
+    renderContext.$root = $root;
+    renderContext.rootComponent = rootComponent;
+    renderContext.currentNode = null;
+    renderContext.beforeNode = null;
+    _render();
+  }
 
   const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
 
