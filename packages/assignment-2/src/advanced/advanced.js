25130a1e43135c67fb468cd275ed5ce86d5a2f66
junilhwang <junil.h@kakao.com>
2024-06-02 01:53:43 +0900

    feat: assignment 2 솔루션 추가

--- packages/assignment-2/src/advanced/advanced.js
+++ packages/assignment-2/src/advanced/advanced.js
@@ -1,3 +1,30 @@
-export const memo1 = (fn) => fn();
+import { shallowEquals } from "../basic/basic.js";
 
-export const memo2 = (fn) => fn();
+export const memo1 = (() => {
+  const cache = new WeakMap();
+  return (fn) => {
+    const cached = cache.get(fn);
+    if (cached) {
+      return cached;
+    }
+    const result = fn();
+    cache.set(fn, result);
+    return result;
+  }
+})();
+
+export const memo2 = (() => {
+  const cache = new WeakMap();
+  const dependenciesCache = new WeakMap();
+  return (fn, dependencies) => {
+    const cached = cache.get(fn);
+    const cachedDependencies = dependenciesCache.get(fn);
+    if (cached && shallowEquals(dependencies, cachedDependencies)) {
+      return cached;
+    }
+    const result = fn();
+    cache.set(fn, result);
+    dependenciesCache.set(fn, dependencies);
+    return result;
+  }
+})();
--- packages/assignment-2/src/basic/basic.js
+++ packages/assignment-2/src/basic/basic.js
@@ -1,24 +1,72 @@
 export function shallowEquals(target1, target2) {
-  return target1 === target2;
+  if (target1 === target2) {
+    return true;
+  }
+  if (Array.isArray(target1) && Array.isArray(target2) && target1.length === target2.length) {
+    return target1.every((v, k) => v === target2[k]);
+  }
+  if (target1.constructor === Object && target2.constructor === Object) {
+    const key1 = Object.keys(target1);
+    const key2 = Object.keys(target2);
+    return target1 === target2 || key1.length === key2.length && key1.every(key => target1[key] === target2[key]);
+  }
+  return false;
 }
 
 export function deepEquals(target1, target2) {
-  return target1 === target2;
+  if (target1 === target2) {
+    return true;
+  }
+  if (Array.isArray(target1) && Array.isArray(target2) && target1.length === target2.length) {
+    return target1.every((v, k) => deepEquals(v, target2[k]));
+  }
+  if (target1.constructor === Object && target2.constructor === Object) {
+    const key1 = Object.keys(target1);
+    const key2 = Object.keys(target2);
+    return target1 === target2 || key1.length === key2.length && key1.every(key => deepEquals(target1[key], target2[key]));
+  }
+  return false;
 }
 
 
 export function createNumber1(n) {
-  return n;
+  return new Number(n);
 }
 
 export function createNumber2(n) {
-  return n;
+  return {
+    toString() { return String(n) }
+  }
 }
 
 export function createNumber3(n) {
-  return n;
+  return {
+    toString() { return String(n) },
+    valueOf() { return n },
+    toJSON() { return `this is createNumber3 => ${n}` },
+  }
 }
 
 export class CustomNumber {
+  static VALUES = {};
+  #value;
+  constructor(value) {
+    if (CustomNumber.VALUES[value]) {
+      return CustomNumber.VALUES[value];
+    }
+    this.#value = value;
+    CustomNumber.VALUES[value] = this;
+  }
 
+  valueOf() {
+    return this.#value.valueOf();
+  }
+
+  toString() {
+    return this.#value.toString();
+  }
+
+  toJSON() {
+    return this.#value.toString();
+  }
 }
