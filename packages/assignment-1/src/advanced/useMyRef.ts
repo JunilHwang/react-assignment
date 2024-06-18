f613e7c905c146c95cb8c810670315018163baa2
Huiju-Kim <huijoo5575@golfzon.com>
2024-06-18 11:36:12 +0900

    [1주차] 심화과제 useRef와 똑같이 동작하는 useMyRef 만들기

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
 export function useMyRef<T>(initValue: T | null) {
-  return { current: initValue }
+  const [ref] = useState(() => ({ current: initValue }));
+  return ref;
 }
