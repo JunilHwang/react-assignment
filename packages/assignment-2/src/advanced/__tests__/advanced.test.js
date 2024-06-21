094184653ff333307a443011eb075bead5eaecef
junilhwang <junil.h@kakao.com>
2024-06-22 00:49:14 +0900

    feat: 2주차 > advanced > 테스트 코드 수정

--- packages/assignment-2/src/advanced/__tests__/advanced.test.js
+++ packages/assignment-2/src/advanced/__tests__/advanced.test.js
@@ -1,10 +1,15 @@
-import { describe } from "vitest";
+import { it, describe, expect } from "vitest";
 import { memo1, memo2 } from "../advanced.js";
 
+let a;
+let b;
+
 describe('assignment 2 > advanced : 값을 캐시하기', () => {
-  it('memo1 > ', () => {
-    let a = 1000000;
-    const fn = () => Array.from({ length: a }).map((_, k) => k + 1);
+  it.each([
+    { fn: () => Array.from({ length: a }).map((_, k) => k + 1) },
+    { fn: () => Array.from({ length: a }).map((_, k) => k * 10) },
+  ])('memo1 $fn > ', ({ fn }) => {
+    a = 1000000;
     expect(fn() === fn()).toBe(false);
     expect(memo1(fn) === memo1(fn)).toBe(true);
     a = 1;
@@ -12,10 +17,12 @@ describe('assignment 2 > advanced : 값을 캐시하기', () => {
     expect(memo1(fn).length).toBe(1000000);
   })
 
-  it('memo2 > ', () => {
-    let a = 100;
-    let b = 100;
-    const fn = () => Array.from({ length: a * b }).map((_, k) => k + 1);
+  it.each([
+    { fn: () => Array.from({ length: a * b }).map((_, k) => k + 1) },
+    { fn: () => Array.from({ length: a * b }).map((_, k) => k * 10) }
+  ])('memo2 $fn > ', ({ fn }) => {
+    a = 100;
+    b = 100;
     expect(fn() === fn()).toBe(false);
     expect(memo2(fn, [a]) === memo2(fn, [a])).toBe(true);
     a = 1;
